55d4ae160fec6d0ea23458278ced95650024480a
Susmaksingh <121927030+Susmaksingh@users.noreply.github.com>
2024-09-19 23:51:50 +0530

    Update GreenSpaceApplication.java

--- src/main/java/com/example/GreenSpace/GreenSpaceApplication.java
+++ src/main/java/com/example/GreenSpace/GreenSpaceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@PropertySource(value = "file:.env", ignoreResourceNotFound = true)
 public class GreenSpaceApplication {
 
 	public static void main(String[] args) {
